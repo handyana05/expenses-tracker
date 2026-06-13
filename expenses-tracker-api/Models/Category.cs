45897f81b2b0a3b1ea941dfaa342c4ef73d48af2
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-13 15:45:32 +0200

    add new models

---
+++ expenses-tracker-api/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿namespace expenses_tracker_api.Models;
+
+public class Category
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = null!;
+    public CategoryType Type { get; set; }
+
+    public Guid UserId { get; set; }
+    public User User { get; set; } = null!;
+    public ICollection<Transaction> Transactions { get; set; } = [];
+}
---
+++ expenses-tracker-api/Models/CategoryType.cs
@@ -0,0 +1,7 @@
+﻿namespace expenses_tracker_api.Models;
+
+public enum CategoryType
+{
+    Income = 1,
+    Expense = 2
+}
