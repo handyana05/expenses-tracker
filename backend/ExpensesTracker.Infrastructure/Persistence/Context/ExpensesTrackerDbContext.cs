204d25be9b715043a987af1566c9cfaf4c0728c2
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-13 19:45:51 +0200

    create dbcontext

---
+++ backend/ExpensesTracker.Infrastructure/Persistence/Context/ExpensesTrackerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ExpensesTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpensesTracker.Infrastructure.Persistence.Context;
+
+public sealed class ExpensesTrackerDbContext(
+    DbContextOptions<ExpensesTrackerDbContext> options) : DbContext(options)
+{
+    public DbSet<User> Users => Set<User>();
+    public DbSet<Category> Categories => Set<Category>();
+    public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(
+            typeof(AssemblyMarker).Assembly);
+    }
+}
