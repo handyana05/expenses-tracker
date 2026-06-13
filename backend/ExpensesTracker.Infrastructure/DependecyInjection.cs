fc8769a3424574361dcf71b1d8a6db532b38ed8a
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-13 20:22:09 +0200

    add servicecolleciton extensions for dependency injection

---
+++ backend/ExpensesTracker.Infrastructure/DependecyInjection.cs
@@ -0,0 +1,28 @@
+﻿using ExpensesTracker.Application.Abstractions.Persistence;
+using ExpensesTracker.Infrastructure.Persistence.Context;
+using ExpensesTracker.Infrastructure.Persistence.Repositories;
+using ExpensesTracker.Infrastructure.Persistence.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpensesTracker.Infrastructure;
+
+public static class DependecyInjection
+{
+    public static IServiceCollection AddInfrastructure(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddDbContext<ExpensesTrackerDbContext>(options =>
+            options.UseNpgsql(
+                configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddScoped<ICategoryRepository, CategoryRepository>();
+        services.AddScoped<ITransactionRepository, TransactionRepository>();
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+        return services;
+    }
+}
