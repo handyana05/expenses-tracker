79db9f6bf704634b6df41814962e423677c53dce
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-13 22:02:47 +0200

    add extension of IServiceCollection on Application project

---
+++ backend/ExpensesTracker.Application/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using ExpensesTracker.Application.Categories.Interfaces;
+using ExpensesTracker.Application.Categories.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ExpensesTracker.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(
+        this IServiceCollection services)
+    {
+        services.AddScoped<ICategoryService, CategoryService>();
+
+        return services;
+    }
+}
