355736b674040b9324c30a50149bfb23e4b0a460
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-13 22:03:23 +0200

    handling CreatedAtUtc and ModifiedAtUrc on SaveChanges of DbContext

--- backend/ExpensesTracker.Infrastructure/Persistence/Context/ExpensesTrackerDbContext.cs
+++ backend/ExpensesTracker.Infrastructure/Persistence/Context/ExpensesTrackerDbContext.cs
@@ -1,4 +1,5 @@
-﻿using ExpensesTracker.Domain.Entities;
+﻿using ExpensesTracker.Domain.Common;
+using ExpensesTracker.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace ExpensesTracker.Infrastructure.Persistence.Context;
@@ -9,6 +10,31 @@ public sealed class ExpensesTrackerDbContext(
     public DbSet<User> Users => Set<User>();
     public DbSet<Category> Categories => Set<Category>();
     public DbSet<Transaction> Transactions => Set<Transaction>();
+
+    public override async Task<int> SaveChangesAsync(
+        CancellationToken cancellationToken = default)
+    {
+        var entries = ChangeTracker
+            .Entries<AuditableEntity>();
+        var utcNow = DateTimeOffset.UtcNow;
+
+        foreach (var entry in entries)
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property(nameof(AuditableEntity.CreatedAtUtc))
+                    .CurrentValue = utcNow;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property(nameof(AuditableEntity.ModifiedAtUtc))
+                    .CurrentValue = utcNow;
+            }
+        }
+
+        return await base.SaveChangesAsync(cancellationToken);
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
