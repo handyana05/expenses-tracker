a86bf8eff745a8a9294a4452f8e55504cbe0160c
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-14 11:47:01 +0200

    create contracts for transaction

---
+++ backend/ExpensesTracker.Api/Transactions/Contracts/UpdateTransactionRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace ExpensesTracker.Api.Transactions.Contracts;
+
+public sealed record UpdateTransactionRequest(
+    Guid CategoryId,
+    decimal Amount,
+    DateTimeOffset TransactionDate,
+    string? Description);
