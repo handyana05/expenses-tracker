4fcd5535a329a3801a3608670394d1039c88dab5
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-14 11:46:43 +0200

    register transaction service

--- backend/ExpensesTracker.Application/DependencyInjection.cs
+++ backend/ExpensesTracker.Application/DependencyInjection.cs
@@ -1,5 +1,7 @@
 ﻿using ExpensesTracker.Application.Categories.Interfaces;
 using ExpensesTracker.Application.Categories.Services;
+using ExpensesTracker.Application.Transactions.Interfaces;
+using ExpensesTracker.Application.Transactions.Services;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace ExpensesTracker.Application;
@@ -10,6 +12,7 @@ public static class DependencyInjection
         this IServiceCollection services)
     {
         services.AddScoped<ICategoryService, CategoryService>();
+        services.AddScoped<ITransactionService, TransactionService>();
 
         return services;
     }
