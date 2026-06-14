b8dd9dd1ac4593e924e5427a2f5bf918aa2cb67c
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-14 10:51:02 +0200

    create mapper for transaction dto

---
+++ backend/ExpensesTracker.Application/Transactions/Mappers/TransactionMapper.cs
@@ -0,0 +1,27 @@
+﻿using ExpensesTracker.Application.Transactions.DTOs;
+using ExpensesTracker.Domain.Entities;
+
+namespace ExpensesTracker.Application.Transactions.Mappers;
+
+public static class TransactionMapper
+{
+    public static TransactionDto ToDto(
+        this Transaction transaction,
+        Category? category = null)
+    {
+        var transactionCategory = category ?? transaction.Category;
+
+        return new TransactionDto(
+            transaction.Id,
+            transaction.CategoryId,
+            transactionCategory.Name,
+            transactionCategory.Type,
+            transaction.Amount,
+            transaction.TransactionDate,
+            transaction.Description);
+    }
+
+    public static IEnumerable<TransactionDto> ToDtos(
+        this IEnumerable<Transaction> transactions)
+        => transactions.Select(t => t.ToDto());
+}
