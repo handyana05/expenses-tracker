0d5ce39833514573ff045c7804aa7526a4df4edf
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-16 16:42:02 +0200

    add extension method to inject authentication jwt bearer

---
+++ backend/ExpensesTracker.Infrastructure/Authentication/AuthenticationDependencyInjection.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+namespace ExpensesTracker.Infrastructure.Authentication;
+
+public static class AuthenticationDependencyInjection
+{
+    public static IServiceCollection AddJwtAuthentication(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var jwtOptions = configuration
+            .GetSection(JwtOptions.SectionName)
+            .Get<JwtOptions>()
+            ?? throw new InvalidOperationException(
+                "JWT configuration is missing.");
+
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ValidIssuer = jwtOptions.Issuer,
+                    ValidAudience = jwtOptions.Audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(
+                        Encoding.UTF8.GetBytes(jwtOptions.SecretKey))
+                };
+            });
+
+        services.AddAuthorization();
+        return services;
+    }
+}
