6f9895edc57854cac6a8258f8871fb159c3b5479
Handyana S. Atmaja <handyana-sumitra.atmaja@adesso.de>
2026-06-16 16:40:56 +0200

    use httpcontext to get current user

--- backend/ExpensesTracker.Infrastructure/Identity/CurrentUser.cs
+++ backend/ExpensesTracker.Infrastructure/Identity/CurrentUser.cs
@@ -1,8 +1,30 @@
 ﻿using ExpensesTracker.Application.Abstractions.Identity;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
 
 namespace ExpensesTracker.Infrastructure.Identity;
 
-public sealed class CurrentUser : ICurrentUser
+public sealed class CurrentUser(
+    IHttpContextAccessor httpContextAccessor) : ICurrentUser
 {
-    public Guid UserId => Guid.Parse("00000000-0000-0000-0000-000000000001");
+    private readonly IHttpContextAccessor _httpContextAccessor = httpContextAccessor;
+
+    public Guid UserId
+    {
+        get
+        {
+            var userId = _httpContextAccessor
+                .HttpContext?
+                .User
+                .FindFirst(ClaimTypes.NameIdentifier)?
+                .Value;
+
+            if (string.IsNullOrWhiteSpace(userId))
+            {
+                throw new UnauthorizedAccessException();
+            }
+
+            return Guid.Parse(userId);
+        }
+    }
 }
